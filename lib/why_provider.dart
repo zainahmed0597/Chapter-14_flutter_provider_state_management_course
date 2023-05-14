d71a669889ff9dfa88c90d714ea0e6340616c122
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2023-05-14 11:07:28 +0500

    update

---
+++ lib/why_provider.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class WhyProviderScreen extends StatefulWidget {
+  const WhyProviderScreen({super.key});
+
+  @override
+  State<WhyProviderScreen> createState() => _WhyProviderScreenState();
+}
+
+class _WhyProviderScreenState extends State<WhyProviderScreen> {
+  int x = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      x++;
+      // print(x);
+      setState(() {
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Build $x');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Why Provider'),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text(
+              DateTime.now().hour.toString() + ":" +
+              DateTime.now().minute.toString() + ":" +
+              DateTime.now().second.toString(),style: TextStyle(
+              fontSize: 50.0,
+            ),
+            ),
+          ),
+          Center(
+            child: Text(
+              '$x'.toString(),
+              style: const TextStyle(
+                fontSize: 50.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
