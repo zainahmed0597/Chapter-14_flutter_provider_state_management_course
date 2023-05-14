93eaef240e4a1052de116ba0f430d927e3f06eb2
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2023-05-14 09:02:39 +0500

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,21 +3,22 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  final int x =100;
+  final int x = 100;
+  final String name = 'Xano';
 
   @override
   Widget build(BuildContext context) {
     print('Build');
     return Scaffold(
       appBar: AppBar(
-        title: Text('Provider Tutorials'),
+        title: const Text('Provider Tutorials'),
         centerTitle: true,
       ),
       body:  Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[Text('Hello Xano! $x'.toString())],
+          children: <Widget>[Text('Hello $name! $x'.toString())],
         ),
       ),
     );
