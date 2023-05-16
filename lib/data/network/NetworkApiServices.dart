cccf1914af793ad355102a87d703431523f67ba2
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2023-05-16 15:34:17 +0500

    add networking setup

---
+++ lib/data/network/NetworkApiServices.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:flutter_provider_state_management_course/data/app_excaptions.dart';
+import 'package:flutter_provider_state_management_course/data/network/BaseApiServices.dart';
+import 'package:http/http.dart';
+
+class NetworkApiServices extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+
+    dynamic responseJson;
+    try {
+      final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      Response response = await post(
+          Uri.parse(url),
+          body: data
+      ).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch(response.statusCode){
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw UnauthorisedException(response.body.toString());
+      default:
+        throw FetchDataException('Error accrued while communicating with server' +
+            'with status code' + response.statusCode.toString());
+    }
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("MVVM App"),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
 
